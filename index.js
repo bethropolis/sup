1760dc63916d0cc1714da8ae259cbd3b685a7152
Bethuel <bethropolis@gmail.com>
2023-03-28 02:49:41 +0000

    #2 log

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 #!/usr/bin/env node
 
 const fs = require('fs-extra');
+const FormData = require('form-data');
+const tar = require('tar');
 const path = require('path');
 const request = require('request');
 const AdmZip = require('adm-zip');
+const axios = require('axios');
+const bcrypt = require('bcrypt');
+const os = require('os');
 const packageJson = require('./package.json');
-const BASE_URL = 'https://bethro.alwaysdata.net/inc/extra';
+const BASE_URL = 'https://bethro.alwaysdata.net/test/plugins';
 
 
 async function downloadPackage(packageName) {
@@ -16,10 +21,11 @@ async function downloadPackage(packageName) {
   let totalBytes = 0;
 
   const request = require('request')(packageUrl);
-
+  let status = true
   request
     .on('response', (response) => {
       if (response.statusCode !== 200) {
+				status = false;
         console.error(`Failed to download package: ${response.statusMessage}`);
         return;
       }
@@ -78,7 +84,120 @@ function installPackage(packageName) {
   });
 }
 
+function initPackage(packageName) {
+  const packageDir = path.join(__dirname, packageName);
 
+  if (fs.existsSync(packageDir)) {
+    console.error(`Package ${packageName} already exists.`);
+    return;
+  }
+
+  console.log(`Creating package ${packageName}...`);
+
+  fs.mkdirSync(packageDir);
+  fs.mkdirSync(path.join(packageDir, 'assets'));
+  fs.mkdirSync(path.join(packageDir, 'libs'));
+  fs.mkdirSync(path.join(packageDir, 'src'));
+
+  fs.writeFileSync(path.join(packageDir, '.signature'), '');
+  fs.writeFileSync(path.join(packageDir, 'main.php'), '');
+  fs.writeFileSync(path.join(packageDir, 'suplike.json'), JSON.stringify({
+    name: packageName,
+    version: '1.0.0',
+    description: '',
+    author: '',
+    dependencies: {},
+  }, null, 2));
+
+  console.log(`Package ${packageName} created successfully!`);
+}
+function publishPackage(packageName, secretKey="aWJvdGFscGx1czRzdHJpbmcK") {
+  const packageDir = path.join(__dirname, packageName);
+  if (!fs.existsSync(packageDir)) {
+    console.error(`Package ${packageName} not found.`);
+    return;
+  }
+
+  // Validate package format
+  const requiredFiles = ['.signature', 'main.php', 'suplike.json'];
+  const packageFiles = fs.readdirSync(packageDir);
+  for (const file of requiredFiles) {
+    if (!packageFiles.includes(file)) {
+      console.error(`Package ${packageName} is missing required file: ${file}`);
+      return;
+    }
+  }
+
+  // Read package data
+  const packageData = fs.readFileSync(path.join(packageDir, 'suplike.json'));
+
+  // Sign package
+  const signature = signPackage(packageData, secretKey);
+  const signaturePath = path.join(packageDir, '.signature');
+  fs.writeFileSync(signaturePath, signature);
+
+  // Create package archive
+  const archivePath = createPackageArchive(packageName);
+
+  // Upload package
+  const form = new FormData();
+  form.append('package', fs.createReadStream(archivePath));
+  form.append('signature', fs.createReadStream(signaturePath));
+  axios.post(`${BASE_URL}/upload`, form, {
+    headers: form.getHeaders()
+  })
+  .then((response) => {
+    console.log(`Package ${packageName} published successfully!`);
+    // Download package files
+    const downloadDir = path.join(__dirname, 'downloads', packageName);
+    fs.mkdirSync(downloadDir, { recursive: true });
+    const packageUrl = `${BASE_URL}/packages/${packageName}.supl`;
+    const packageFilePath = path.join(downloadDir, `${packageName}.supl`);
+    axios({
+      method: 'get',
+      url: packageUrl,
+      responseType: 'stream'
+    })
+    .then((response) => {
+      response.data.pipe(fs.createWriteStream(packageFilePath));
+      response.data.on('end', () => {
+        console.log(`Package ${packageName} downloaded successfully!`);
+      });
+    })
+    .catch((error) => {
+      console.error(`Failed to download package: ${error.message}`);
+    });
+  })
+  .catch((error) => {
+    console.error(`Failed to publish package: ${error.message}`);
+  });
+}
+
+function createPackageArchive(packageName) {
+  const packageDir = path.join(__dirname, packageName);
+  const archivePath = path.join(os.tmpdir(), `${packageName}.supl`);
+  const output = fs.createWriteStream(archivePath);
+  const archive = require('archiver')('zip', { zlib: { level: 9 } });
+
+  archive.on('error', (err) => {
+    throw err;
+  });
+
+  archive.pipe(output);
+
+  archive.directory(packageDir, packageName);
+
+  archive.finalize();
+
+  return archivePath;
+}
+
+function signPackage(packageData, secretKey) {
+  const bcrypt = require('bcrypt');
+  const salt = bcrypt.genSaltSync(10);
+  const hash = bcrypt.hashSync(packageData, salt);
+  return hash;
+}
 function removePackage(packageName) {
   const packageDir = path.join(__dirname, packageName);
   const packageSupl = path.join(__dirname, `${packageName}.supl`);
@@ -133,6 +252,12 @@ switch (command) {
       console.log('Please specify a package name to remove.');
     }
     break;
+		case 'init':
+		 initPackage(packageName)
+		break;
+		case 'publish':
+		 publishPackage(packageName)
+		break;
   default:
     console.error(`Invalid command: ${command}`);
     console.error('Usage: sup [install|remove] <package>');
